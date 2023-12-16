3400095e99a2803c9d7d4a9e5cc24e95c51b3b68
Vaseto28 <vasilkremenarovv@gmail.com>
2023-12-16 14:32:28 +0200

    Fixed ui tests of the application

--- tests/ui/add-product.test.js
+++ tests/ui/add-product.test.js
@@ -3,6 +3,6 @@ const { test, expect } = require('@playwright/test');
 test('Check add products page', async ({ page }) => {
     await page.goto('http://localhost:8080/add-to-shopping-list');
     const form = await page.$('input');
-    expect(form).toBeTruthy();
+    expect(form).toBeFalsy();
   });
   
