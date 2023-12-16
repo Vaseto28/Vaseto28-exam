53a20f51f17baf87dfe1f81d191e1ef673fcba60
Vaseto28 <vasilkremenarovv@gmail.com>
2023-12-16 15:08:05 +0200

    Changed the url at the ui tests

--- tests/ui/shoppingList.test.js
+++ tests/ui/shoppingList.test.js
@@ -1,7 +1,7 @@
 const { test, expect } = require('@playwright/test');
 
 test('Check shopping list page', async ({ page }) => {
-    await page.goto('http://localhost:8080/shopping-list');
+    await page.goto('https://vaseto28-regular-exam.onrender.com/shopping-list');
     const list = await page.$('dl');
     expect(list).toBeFalsy();
   });
